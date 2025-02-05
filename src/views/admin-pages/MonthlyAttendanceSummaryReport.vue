596b8cebe89ea7da29dfb9a28121f6d7c7fdfdda
MKRashed <mkrashed757@gamil.com>
2025-02-05 18:41:02 +0600

    Update Monthly Attendance Summary Report: enhance table layout, add designation column, and improve attendance metrics

--- src/views/admin-pages/MonthlyAttendanceSummaryReport.vue
+++ src/views/admin-pages/MonthlyAttendanceSummaryReport.vue
@@ -73,48 +73,80 @@ const goBack = () => router.go(-1)
       <div class="overflow-x-auto card-bg">
         <table class="min-w-full table-auto border-collapse border border-gray-300 bg-white">
           <thead>
-            <tr class="bg-gray-200 text-xs">
-              <th class="border p-1">Emp/Worker Name</th>
-              <th class="border p-1">Day</th>
-              <th class="border p-1">Total Present</th>
-              <th class="border p-1">Total Absent</th>
-              <th class="border p-1">Remain Absent</th>
-              <th class="border p-1">Total Weekend</th>
-              <th class="border p-1">Actual Late Day</th>
-              <th class="border p-1">Actual Late Hour</th>
-              <th class="border p-1">Actual Early Day</th>
-              <th class="border p-1">Actual Early Hour</th>
-              <th class="border p-1">Shift Hour</th>
-              <th class="border p-1">Working Hour</th>
-              <th class="border p-1">OT Hour</th>
-              <th class="border p-1">Short Leave Day</th>
-              <th class="border p-1">Leave Day</th>
-              <th class="border p-1">Total Present In Weekend</th>
+            <tr class="bg-gray-100 text-sm">
+              <th rowspan="2" class="border px-1 py-0.5">Employee Name</th>
+              <th rowspan="2" class="border px-1 py-0.5">Designation</th>
+              <th colspan="4" class="border px-1 py-0.5">Attendance Summary</th>
+              <!-- <th rowspan="2" class="border px-1 py-0.5">Total Present</th>
+              <th rowspan="2" class="border px-1 py-0.5">Total Absent</th>
+              <th rowspan="2" class="border px-1 py-0.5">Remain Absent</th>
+              <th rowspan="2" class="border px-1 py-0.5">Total Weekend</th> -->
+              <th colspan="2" class="border px-1 py-0.5">Actual Late</th>
+              <th colspan="2" class="border px-1 py-0.5">Remaining Late</th>
+              <th colspan="2" class="border px-1 py-0.5">Actual Early</th>
+              <th colspan="2" class="border px-1 py-0.5">Remaining Early</th>
+              <!-- <th rowspan="2" class="border px-1 py-0.5">Shift Hour</th> -->
+              <th rowspan="2" class="border px-1 py-0.5">Working Hour</th>
+              <th rowspan="2" class="border px-1 py-0.5">OT Hour</th>
+              <th colspan="4" class="border px-1 py-0.5">Leave Day</th>
+              <th colspan="2" class="border px-1 py-0.5">Short Leave</th>
+              <th rowspan="2" class="border px-1 py-0.5">Duty in Weekend</th>
+              <th rowspan="2" class="border px-2 py-0.5">Comment</th>
+            </tr>
+            <tr class="bg-gray-100 text-xs">
+              <th class="border px-2 py-0.5">TD</th>
+              <th class="border px-2 py-0.5">TP</th>
+              <th class="border px-2 py-0.5">TW</th>
+              <th class="border px-2 py-0.5">TA</th>
+              <th class="border px-1 py-0.5">Day</th>
+              <th class="border px-2 py-0.5">Hour</th>
+              <th class="border px-1 py-0.5">Day</th>
+              <th class="border px-2 py-0.5">Hour</th>
+              <th class="border px-1 py-0.5">Day</th>
+              <th class="border px-2 py-0.5">Hour</th>
+              <th class="border px-2 py-0.5">Day</th>
+              <th class="border px-2 py-0.5">Hour</th>
+              <th class="border px-2 py-0.5">CL</th>
+              <th class="border px-2 py-0.5">ML</th>
+              <th class="border px-2 py-0.5">SL</th>
+              <th class="border px-2 py-0.5">WPL</th>
+              <th class="border px-2 py-0.5">Entry</th>
+              <th class="border px-2 py-0.5">Exist</th>
             </tr>
           </thead>
-          <tbody class="text-center text-xs">
-            <tr v-for="log in monthly_company_summary" :key="log?.date">
-              <td class="border px-1 py-0.5">{{ log?.user }}</td>
-              <td class="border px-1 py-0.5">{{ log?.total_monthly_days }}</td>
-              <td class="border px-1 py-0.5">{{ log?.total_present }}</td>
-              <td class="border px-1 py-0.5">{{ log?.total_absent }}</td>
-              <td class="border px-1 py-0.5">{{ log?.remain_total_absent }}</td>
-              <td class="border px-1 py-0.5">{{ log?.total_weekend }}</td>
-              <td class="border px-1 py-0.5">{{ log?.actual_late_day }}</td>
-              <td class="border px-1 py-0.5">{{ log?.actual_late_hour }}</td>
-              <td class="border px-1 py-0.5">{{ log?.actual_early_day }}</td>
-              <td class="border px-1 py-0.5">{{ log?.actual_early_hour }}</td>
-              <td class="border px-1 py-0.5">{{ log?.total_shift_hour }}</td>
-              <td class="border px-1 py-0.5">{{ log?.total_working_hours }}</td>
-              <td class="border px-1 py-0.5">{{ log?.total_overtime_hours }}</td>
-              <td class="border px-1 py-0.5">{{ log?.total_short_leave }}</td>
-              <td class="border px-1 py-0.5">
-                <div v-if="log?.total_cl_leave">{{ log?.total_cl_leave }}CL</div>
-                <div v-if="log?.total_ml_leave">{{ log?.total_ml_leave }}ML</div>
-                <div v-if="log?.total_sl_leave">{{ log?.total_sl_leave }}SL</div>
-                <div v-if="log?.total_wpl_leave">{{ log?.total_wpl_leave }}WPL</div>
+          <tbody class="text-center text-sm">
+            <tr
+              v-for="log in monthly_company_summary"
+              :key="log?.date"
+              class="hover:bg-blue-500 hover:text-white"
+            >
+              <td class="border px-2 py-0.5">{{ log?.user }}</td>
+              <td class="border px-2 py-0.5">{{ log?.designation }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_monthly_days }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_present }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_weekend }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_absent }}</td>
+              <!-- <td class="border px-2 py-0.5">{{ log?.remain_total_absent }}</td> -->
+              <td class="border px-2 py-0.5">{{ log?.actual_late_day }}</td>
+              <td class="border px-2 py-0.5">{{ log?.actual_late_hour }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_remain_late_day }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_remain_late_hour }}</td>
+              <td class="border px-2 py-0.5">{{ log?.actual_early_day }}</td>
+              <td class="border px-2 py-0.5">{{ log?.actual_early_hour }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_remain_early_day }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_remain_early_hour }}</td>
+              <td class="border px-2 py-0.5">
+                {{ log?.total_working_hours || 0 }} / {{ log?.total_shift_hour || 0 }}
               </td>
-              <td class="border px-1 py-0.5">{{ log.total_present_in_weekend }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_overtime_hours }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_cl_leave }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_ml_leave }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_sl_leave }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_wpl_leave }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_first_short_leave }}</td>
+              <td class="border px-2 py-0.5">{{ log?.total_last_short_leave }}</td>
+              <td class="border px-2 py-0.5">{{ log.total_present_in_weekend }}</td>
+              <td class="border px-2 py-0.5"></td>
             </tr>
           </tbody>
         </table>
