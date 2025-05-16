9c0498997c9d68fcbecb53ab6a1ba490d7156ac7
msisaif <msi132043@gmail.com>
2025-05-17 02:00:26 +0600

    refactor: streamline ManualAttendanceShow component by removing unused modals and simplifying approval logic

--- src/views/private-pages/ManualAttendanceShow.vue
+++ src/views/private-pages/ManualAttendanceShow.vue
@@ -1,19 +1,16 @@
 <script setup>
+import ApprovalItem from '@/components/applications/ApprovalItem.vue'
 import LoaderView from '@/components/common/LoaderView.vue'
 import ShareComponent from '@/components/common/ShareComponent.vue'
 import { useAuthStore } from '@/stores/auth'
 import { useManualAttendanceStore } from '@/stores/manual-attendance'
-import { computed, onMounted, ref } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 const router = useRouter()
 const route = useRoute()
 const manualAttendanceStore = useManualAttendanceStore()
 const authStore = useAuthStore()
-const approvalModal = ref(false)
-const approvalNote = ref('')
-const rejectionModal = ref(false)
-const rejectionReason = ref('')
 
 const loading = computed(() => manualAttendanceStore.loading)
 const manualAttendance = computed(() => manualAttendanceStore.manualAttendance)
@@ -27,58 +24,14 @@ onMounted(async () => {
   }
 })
 
-let action = ref('')
-
-function openApprovalModal(url) {
-  action.value = url
-  approvalModal.value = true
-}
-
-async function submitApproval() {
-  acceptManualAttendanceAction(action.value)
-  approvalModal.value = false
-}
-
-const rejectManualAttendance = async () => {
-  try {
-    await manualAttendanceStore.rejectManualAttendance(route.params.id, rejectionReason.value)
-    alert('Manual attendance rejected successfully!')
-    rejectionModal.value = false
-    rejectionReason.value = ''
-    await manualAttendanceStore.fetchManualAttendanceById(route.params.id)
-  } catch (err) {
-    console.error('Failed to reject manual attendance:', err)
-    alert('Failed to reject manual attendance.')
-  }
-}
-
-const openRejectionModal = () => {
-  rejectionModal.value = true
-}
-
-const acceptManualAttendanceAction = async (action) => {
-  try {
-    const { id } = route.params
-    if (action === 'inCharge') {
-      await manualAttendanceStore.inChargeAccept({ id, note: approvalNote.value })
-    }
-    if (action === 'recommend') {
-      await manualAttendanceStore.recommendByAccept({ id, note: approvalNote.value })
-    }
-    if (action === 'approve') {
-      await manualAttendanceStore.approvedByAccept({ id, note: approvalNote.value })
-    }
-
-    await manualAttendanceStore.fetchManualAttendanceById(id)
-  } catch (err) {
-    console.error(`Failed to accept ${action}:`, err)
-  }
-}
-
 const goBack = () => router.go(-1)
 
 function print() {
   window.print()
+}
+
+const onAction = async () => {
+  manualAttendanceStore.fetchManualAttendanceById(route.params.id)
 }
 </script>
 
@@ -155,11 +108,16 @@ function print() {
         <div class="pt-10">
           <div>
             <hr class="w-44 border-black" />
-            <div><b>Applicant</b></div>
-            <p class="font-medium">{{ manualAttendance?.user.name }}</p>
+            <p>
+              Name: <b>{{ manualAttendance?.user.name }}</b>
+            </p>
             <div class="text-sm">
-              <p>{{ manualAttendance?.user?.department?.name }}</p>
-              <p>{{ manualAttendance?.user?.designation?.title }}</p>
+              <p>
+                Designation: <b>{{ manualAttendance?.user?.designation?.title }}</b>
+              </p>
+              <p>
+                Department: <b>{{ manualAttendance?.user?.department?.name }}</b>
+              </p>
             </div>
           </div>
         </div>
@@ -187,186 +145,32 @@ function print() {
         </div>
       </div>
 
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div class="pt-10">
-          <p v-if="manualAttendance?.in_charge_user">
-            {{ manualAttendance?.in_charge_user?.name || '' }}
-          </p>
-          <p v-else>
-            {{ manualAttendance?.user?.other_approval?.in_charge_user?.name || '' }}
-          </p>
+      <div
+        v-if="manualAttendance"
+        class="grid grid-cols-2 text-sm md:text-base md:grid-cols-3 md:gap-x-4 gap-y-14 pt-14 items-end"
+      >
+        <ApprovalItem
+          :application="manualAttendance"
+          type="manual_attendance_applications"
+          item="in_charge"
+          :onAction="onAction"
+        />
 
-          <div
-            v-if="
-              manualAttendance?.status !== 'Rejected' &&
-              manualAttendance?.status !== 'Approved' &&
-              !manualAttendance?.in_charge_user_id &&
-              manualAttendance?.user?.other_approval?.in_charge_user_id === authStore?.user?.id
-            "
-            class="print:hidden"
-          >
-            <p class="text-xs text-blue-600">
-              {{ manualAttendance?.user?.name }} has submitted an "attendance request". <br />
-              Will you forward it?
-            </p>
-            <div class="flex gap-4">
-              <button
-                class="font-bold text-lg text-green-600"
-                @click="openApprovalModal('inCharge')"
-              >
-                ✔
-              </button>
-              <button class="" @click="openRejectionModal">❌</button>
-            </div>
-          </div>
+        <ApprovalItem
+          :application="manualAttendance"
+          type="manual_attendance_applications"
+          item="recommend_by"
+          :onAction="onAction"
+        />
 
-          <hr class="w-44 border-black" />
-          <p class="font-bold">
-            In-Charge
-            <span v-if="manualAttendance?.in_charge_user_id" class="text-green-600">(✔)</span>
-            <span
-              v-if="
-                manualAttendance?.user?.other_approval?.in_charge_user &&
-                !manualAttendance?.in_charge_user_id
-              "
-              class="pl-2 text-yellow-700"
-              ><i class="fad fa-spinner"></i
-            ></span>
-          </p>
-          <p class="text-xs text-gray-500">
-            {{ manualAttendance?.in_charge_note }}
-          </p>
-        </div>
-
-        <div class="pt-10">
-          <p v-if="manualAttendance?.recommend_by_user">
-            {{ manualAttendance?.recommend_by_user?.name || '' }}
-          </p>
-          <p v-else>
-            {{ manualAttendance?.user?.other_approval?.recommend_by_user?.name || 'N/A' }}
-          </p>
-          <div
-            v-if="
-              manualAttendance?.status !== 'Rejected' &&
-              manualAttendance?.status !== 'Approved' &&
-              !manualAttendance?.recommend_by_user_id &&
-              manualAttendance?.user?.other_approval?.recommend_by_user_id === authStore.user?.id
-            "
-            class="print:hidden"
-          >
-            <p class="text-xs text-blue-600">
-              {{ manualAttendance?.user?.name }} has submitted an "attendance request".<br />
-              Will you recommend it?
-            </p>
-            <div class="flex gap-4">
-              <button
-                class="font-bold text-lg text-green-600"
-                @click="openApprovalModal('recommend')"
-              >
-                ✔
-              </button>
-              <button class="" @click="openRejectionModal">❌</button>
-            </div>
-          </div>
-          <hr class="w-44 border-black" />
-          <p class="font-bold">
-            Recommend By
-            <span v-if="manualAttendance?.recommend_by_user_id" class="text-green-600">(✔)</span>
-            <span
-              v-if="
-                manualAttendance?.user?.other_approval?.recommend_by_user &&
-                !manualAttendance?.recommend_by_user_id
-              "
-              class="pl-2 text-yellow-700"
-              ><i class="fad fa-spinner"></i
-            ></span>
-          </p>
-          <p class="text-xs text-gray-500">
-            {{ manualAttendance?.recommend_by_note }}
-          </p>
-        </div>
-      </div>
-
-      <div class="flex flex-col pt-10">
-        <p v-if="manualAttendance?.approved_by_user">
-          {{ manualAttendance?.approved_by_user?.name || '' }}
-        </p>
-        <p v-else>
-          {{ manualAttendance?.user?.other_approval?.approved_by_user?.name || '' }}
-        </p>
-
-        <div
-          v-if="
-            manualAttendance?.status !== 'Rejected' &&
-            manualAttendance?.status !== 'Approved' &&
-            !manualAttendance?.approved_by_user_id &&
-            manualAttendance?.user?.other_approval?.approved_by_user_id === authStore.user?.id
-          "
-          class="print:hidden"
-        >
-          <p class="text-xs text-blue-600">
-            {{ manualAttendance?.user?.name }} has submitted an "attendance request".<br />
-            Will you accept it?
-          </p>
-          <div class="flex gap-4">
-            <button class="font-bold text-lg text-green-600" @click="openApprovalModal('approve')">
-              ✔
-            </button>
-            <button class="" @click="openRejectionModal">❌</button>
-          </div>
-        </div>
-
-        <hr class="w-44 border-black" />
-        <p class="font-bold">
-          Approved By
-          <span v-if="manualAttendance?.approved_by_user_id" class="text-green-600">(✔)</span>
-          <span
-            v-if="
-              manualAttendance?.user?.other_approval?.approved_by_user &&
-              !manualAttendance?.approved_by_user_id
-            "
-            class="pl-2 text-yellow-700"
-            ><i class="fad fa-spinner"></i
-          ></span>
-        </p>
-        <p class="text-xs text-gray-500">
-          {{ manualAttendance?.approved_by_note }}
-        </p>
+        <ApprovalItem
+          :application="manualAttendance"
+          type="manual_attendance_applications"
+          item="approved_by"
+          :onAction="onAction"
+        />
       </div>
     </div>
     <ShareComponent />
-  </div>
-
-  <div v-if="approvalModal" class="modal-bg">
-    <div class="modal-card">
-      <h3 class="title-lg">Accept Application</h3>
-      <input
-        v-model="approvalNote"
-        rows="4"
-        placeholder="Enter accept note..."
-        class="w-full border rounded-lg p-2 text-gray-700"
-      />
-      <div class="flex justify-end gap-2 mt-4">
-        <button class="btn-3" @click="approvalModal = false">Cancel</button>
-        <button class="btn-2 bg-red-500 text-white" @click="submitApproval">Confirm</button>
-      </div>
-    </div>
-  </div>
-
-  <!-- Rejection Modal -->
-  <div v-if="rejectionModal" class="modal-bg">
-    <div class="modal-card">
-      <h3 class="title-lg">Reject Manual Attendance</h3>
-      <textarea
-        v-model="rejectionReason"
-        rows="4"
-        placeholder="Enter rejection reason..."
-        class="w-full border rounded-lg p-2 text-gray-700"
-      ></textarea>
-      <div class="flex justify-end gap-2 mt-4">
-        <button class="btn-3" @click="rejectionModal = false">Cancel</button>
-        <button class="btn-2 bg-red-500 text-white" @click="rejectManualAttendance">Confirm</button>
-      </div>
-    </div>
   </div>
 </template>
