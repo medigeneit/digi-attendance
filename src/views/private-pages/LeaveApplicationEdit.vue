c585c6ec4b1b1c742d2d0baa9ebd8313d6f42119
msisaif <msi313.bd@gmail.com>
2025-05-19 19:24:56 +0600

    feat: refine user fetching logic in LeaveApplicationEdit to use type-based filtering and improve form handling

--- src/views/private-pages/LeaveApplicationEdit.vue
+++ src/views/private-pages/LeaveApplicationEdit.vue
@@ -22,10 +22,10 @@ const selectUser = ref(null)
 const form = ref({
   user_id: '',
   last_working_date: '',
-  resumption_date:  '',
+  resumption_date: '',
   reason: '',
   works_in_hand: '',
-  handover_user_id:  '',
+  handover_user_id: '',
   leave_days: [],
 })
 
@@ -41,17 +41,22 @@ onMounted(async () => {
     }
 
     if (leaveApplicationStore.leaveApplication) {
-      await userStore.fetchTypeWiseEmployees({ except: [leaveApplicationStore.leaveApplication.user_id] })
-      if(userStore.users.length > 0)
-      {
-        
-        const user = computed(() => userStore.users.filter((user) => user.id === leaveApplicationStore.leaveApplication?.handover_user_id)[0] || {})
-        console.log('users', user.value);
+      await userStore.fetchTypeWiseEmployees({
+        type: leaveApplicationStore.leaveApplication.user.type,
+        except: [leaveApplicationStore.leaveApplication.user.id],
+      })
+      if (userStore.users.length > 0) {
+        const user = computed(
+          () =>
+            userStore.users.filter(
+              (user) => user.id === leaveApplicationStore.leaveApplication?.handover_user_id,
+            )[0] || {},
+        )
+        console.log('users', user.value)
         selectUser.value = user.value
       }
     }
-    if(leaveApplicationStore.leaveApplication)
-    {
+    if (leaveApplicationStore.leaveApplication) {
       form.value.user_id = leaveApplicationStore.leaveApplication?.user_id
       form.value.last_working_date = leaveApplicationStore.leaveApplication?.last_working_date
       form.value.resumption_date = leaveApplicationStore.leaveApplication?.resumption_date
@@ -193,8 +198,7 @@ const submitLeaveApplication = async () => {
       leave_days: leaveDaysPayload,
       json_data: leaveDaysJson,
     }
-    if(payload.id)
-    {
+    if (payload.id) {
       const newApplication = await leaveApplicationStore.updateLeaveApplication(payload.id, payload)
       if (newApplication) {
         router.push({ name: 'LeaveApplicationShow', params: { id: newApplication?.id } })
