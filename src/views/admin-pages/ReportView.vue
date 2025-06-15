a935b4cb01d1dc69f1356d133d7f65c23cb46eca
MKRashed <mkrashed757@gamil.com>
2025-06-15 18:54:41 +0600

    feat: add Daily and Monthly Late Attendance Reports with updated routing and data handling

--- src/views/admin-pages/ReportView.vue
+++ src/views/admin-pages/ReportView.vue
@@ -5,9 +5,13 @@
       <i class="fas fa-calendar-alt text-3xl"></i>
       Daily Attendance Report
     </RouterLink>
-    <RouterLink :to="{ name: 'LateAttendanceReport' }" class="main-button">
+    <RouterLink :to="{ name: 'DailyLateAttendanceReport' }" class="main-button">
       <i class="far fa-file-chart-line text-3xl"></i>
       Daily Late Reports
+    </RouterLink>
+    <RouterLink :to="{ name: 'LateAttendanceReport' }" class="main-button">
+      <i class="far fa-file-chart-line text-3xl"></i>
+      Monthly Late Reports
     </RouterLink>
     <RouterLink :to="{ name: 'AttendanceSummaryReport' }" class="main-button">
       <i class="fas fa-calendar-day text-3xl"></i>
