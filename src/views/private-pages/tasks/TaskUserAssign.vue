432236c50843b1c1409a3f437388c0cd8034c0d6
saifuddin24 <saif.bigc@gmail.com>
2025-06-28 10:37:39 +0600

    UserChip is set  to task user assign

--- src/views/private-pages/tasks/TaskUserAssign.vue
+++ src/views/private-pages/tasks/TaskUserAssign.vue
@@ -1,5 +1,6 @@
 <script setup>
 import MultiselectDropdown from '@/components/MultiselectDropdown.vue'
+import UserChip from '@/components/user/UserChip.vue'
 import { useTaskStore } from '@/stores/useTaskStore'
 import { useUserStore } from '@/stores/user'
 import { computed, onMounted, ref } from 'vue'
@@ -14,11 +15,18 @@ const user_ids = computed(() => selectedUsers.value.map((u) => u.id))
 const taskId = route.params.id
 const loading = ref(false)
 const error = ref(null)
+const users = ref([])
 
 onMounted(async () => {
   loading.value = true
-  await taskStore.fetchTask(taskId)
-  await userStore.fetchUsers() // all available users
+  await taskStore.fetchTask(taskId, { with_parent: 'true' })
+  if (taskStore.task.parent) {
+    users.value = taskStore.task?.parent?.users || []
+  } else {
+    await userStore.fetchUsers() // all available users
+    users.value = userStore.users
+  }
+
   selectedUsers.value = taskStore.task.users
   loading.value = false
 })
@@ -52,12 +60,33 @@ const submit = async () => {
           <label class="block text-gray-700 font-medium mb-2">Select Users</label>
           <MultiselectDropdown
             v-model="selectedUsers"
-            :options="userStore.users"
+            :options="users"
             :multiple="true"
             track-by="id"
             label="label"
             placeholder="Select users"
-          />
+          >
+            <template #selection="{ ...attrs }">
+              <div class="mb-2 flex flex-wrap gap-2">
+                <UserChip v-for="user in attrs?.values || []" :key="user.id" :user="user">
+                  <template #after="{ user }">
+                    <button
+                      class="size-6 border rounded-full hover:bg-red-400 hover:text-white"
+                      @click.prevent="
+                        selectedUsers = selectedUsers.filter((su) => su.id !== user.id)
+                      "
+                    >
+                      &times;
+                    </button>
+                  </template>
+                </UserChip>
+              </div>
+              <!-- <pre>{{ attrs }}</pre> -->
+            </template>
+            <template #option="{ option: user }">
+              <UserChip :user="user" />
+            </template>
+          </MultiselectDropdown>
         </div>
 
         <div v-if="error" class="mb-4 text-red-500 font-medium">
