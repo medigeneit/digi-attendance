881bb9b5be25dcb1342f526770292b0352f1538e
saifuddin24 <saif.bigc@gmail.com>
2025-07-05 10:53:52 +0600

    refactor: update TaskProgressTable props and remove unused imports in TaskShow

--- src/components/tasks/TaskProgressTable.vue
+++ src/components/tasks/TaskProgressTable.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-import type { TaskUserWithProgress } from '@/interfaces/user'
+import type { Task } from '@/interfaces/task'
+import type { TaskUser, TaskUserWithProgress } from '@/interfaces/user'
 import { getDisplayDate } from '@/libs/datetime'
 import UserChip from '../user/UserChip.vue'
 
 const props = defineProps<{
   progressUsers: TaskUserWithProgress[]
+  taskUsers: TaskUser[]
+  task: Task
+  tasks: Task[]
 }>()
 </script>
 
@@ -21,12 +25,15 @@ const props = defineProps<{
         </th>
         <th class="px-2 py-1 border-y font-semibold text-xs uppercase text-center">Started</th>
         <th class="px-2 py-1 border-y font-semibold text-xs uppercase text-center">Finished</th>
-        <th class="px-2 py-1 border-y font-semibold text-xs uppercase text-center">
-          User Progress
-        </th>
-        <th class="px-2 py-1 border-y font-semibold text-xs uppercase text-center">
-          Task Progress
-        </th>
+        <th class="px-2 py-1 border-y font-semibold text-xs uppercase text-center">Progress</th>
+        <!-- 
+          <th class="px-2 py-1 border-y font-semibold text-xs uppercase text-center">
+            User Progress
+          </th>
+          <th class="px-2 py-1 border-y font-semibold text-xs uppercase text-center">
+            Task Progress
+          </th> 
+        -->
       </tr>
     </thead>
     <tbody>
@@ -36,17 +43,27 @@ const props = defineProps<{
         class="border-t hover:bg-gray-50 cursor-pointer"
       >
         <td class="px-2 py-1 border-y">{{ index + 1 }}</td>
+
         <td class="px-2 py-1 border-y">
           <UserChip :user="progressUser" />
         </td>
+
         <td class="px-2 py-1 border-y text-center">
           {{ getDisplayDate(progressUser.started_at) }}
         </td>
+
         <td class="px-2 py-1 border-y text-center">
           {{ getDisplayDate(progressUser.finished_at) }}
         </td>
-        <td class="px-2 py-1 border-y text-center">{{ progressUser.user_progress }}%</td>
-        <td class="px-2 py-1 border-y text-center">{{ progressUser.task_progress }}%</td>
+
+        <td class="px-2 py-1 border-y text-center">
+          {{ getDisplayDate(progressUser.finished_at) }}
+        </td>
+
+        <!-- 
+          <td class="px-2 py-1 border-y text-center">{{ progressUser.user_progress }}%</td>
+          <td class="px-2 py-1 border-y text-center">{{ progressUser.task_progress }}%</td> 
+        -->
       </tr>
     </tbody>
   </table>
