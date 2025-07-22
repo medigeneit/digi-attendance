20de49d3205ef52fc35f18bd8b236db62c9e4230
msisaif <msisaif@users.noreply.github.com>
2025-07-22 11:53:24 +0000

    deploy: 7d8bc3c9f56d3f288c67cc51d4689dbdef00d1b5

--- assets/requirement-BTnUbB1A.js
+++
@@ -1,1 +0,0 @@
-import{C as n}from"./index-DAvUzAED.js";async function i(e){return n.post("/requirements",e)}async function a(e,{params:r={}}={}){return n.get(`/requirements/${e}`,{params:r})}export{i as a,a as f};
