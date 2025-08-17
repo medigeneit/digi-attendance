ebfb5df154ffbc21480647fbde9c880c4ca1a044
saifuddin24 <saifuddin24@users.noreply.github.com>
2025-08-17 14:01:42 +0000

    deploy: 7f9a43fcfb8f74a7a6ba8bd58671f69e7b45aaff

---
+++ assets/tags-BDB8RrMj.js
@@ -0,0 +1,1 @@
+import{D as r,J as n,r as g}from"./index-DM-YVLMB.js";async function o(a){return r.get("/tags",{params:{tag_type:a}})}const i=n("tags",()=>{const a=g([]);return{tags:a,fetchTags:async e=>{var t,s;a.value=((s=(t=await o(e))==null?void 0:t.data)==null?void 0:s.tags)||[]}}});export{i as u};
