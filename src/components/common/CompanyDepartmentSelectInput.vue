5ab457f573038403315a87dd0323f7a16777bbf3
saifuddin24 <saif.bigc@gmail.com>
2025-08-27 16:40:59 +0600

    Option grouping is in SelectDropdown component

--- src/components/common/CompanyDepartmentSelectInput.vue
+++ src/components/common/CompanyDepartmentSelectInput.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed } from 'vue'
+import SelectDropdown from '../SelectDropdown.vue'
 
 const props = defineProps({
   companies: { type: Array, required: true },
@@ -28,21 +29,22 @@ const department_id = computed({
 <template>
   <div>
     <slot name="label">Department</slot>
-    <select
+    <SelectDropdown
+      :options="companies"
       v-model="department_id"
+      :isOptionGroup="true"
+      optGroupOptionKey="departments"
+      label="name"
       class="w-full border rounded-md focus:ring-2 focus:ring-blue-500"
       :class="className.select"
+      clearable
     >
-      <option value="">{{ defaultOption }}</option>
-      <optgroup v-for="company in companies" :key="company.id" :label="company.name">
-        <option
-          v-for="department in company.departments"
-          :value="department.id"
-          :key="department.id"
-        >
-          {{ department.name }}
-        </option>
-      </optgroup>
-    </select>
+      <template #selected-option="{ option, group }">
+        <div>
+          <span v-if="option?.name" class="text-gray-800 line-clamp-1">{{ option?.name }}</span>
+          <span v-else class="text-gray-500">{{ defaultOption }}</span>
+        </div>
+      </template>
+    </SelectDropdown>
   </div>
 </template>
