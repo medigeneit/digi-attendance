d79f7338b8b47af3b10ad82ed38ee578921bc6be
saifuddin24 <saif.bigc@gmail.com>
2025-10-19 19:12:47 +0600

    update router link to point to RequirementTaskShow for task details

--- src/components/requirements/RequirementDetailTableRow.vue
+++ src/components/requirements/RequirementDetailTableRow.vue
@@ -132,7 +132,7 @@ function handleCreateAssignButtonClick() {
       <template v-if="task.id">
         <td class="border-2 border-gray-700 p-3 text-center">
           <RouterLink
-            :to="{ name: 'TaskShow', params: { id: task.id } }"
+            :to="{ name: 'RequirementTaskShow', params: { id: task.id } }"
             class="hover:underline text-gray-900 hover:text-blue-600 whitespace-nowrap block"
             target="_blank"
           >
