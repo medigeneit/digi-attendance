2b15a0a63cdcfb02fc33efcea45a47c6b4263087
MKRashed <MKRashed@users.noreply.github.com>
2025-11-04 12:28:11 +0000

    deploy: 3beb624d5ff20db0ef3179411a93ce4c9e1f24e4

--- assets/AboutView-Da3Q8Is6.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as t,o as s,a}from"./index-C2c6jDtn.js";const r={},n={class:"about"};function c(i,e){return s(),t("div",n,e[0]||(e[0]=[a("h1",null,"This is an about page",-1)]))}const l=o(r,[["render",c]]);export{l as default};
