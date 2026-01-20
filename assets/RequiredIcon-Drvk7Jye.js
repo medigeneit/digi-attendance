327a2ff6318d957fc4c082b14a1a82181099c671
MKRashed <MKRashed@users.noreply.github.com>
2026-01-20 06:24:08 +0000

    deploy: cd7986f9d4dcfef26a561b35c5f5d79fb33e6202

---
+++ assets/RequiredIcon-Drvk7Jye.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./index-gP12q8ha.js";const o={},s={class:"font-medium text-red-600 text-sm"};function n(r,_){return c(),t("span",s,"*")}const m=e(o,[["render",n]]);export{m as R};
