77405c75456292aa7a0a5547ae3a8f232e09a48d
MKRashed <MKRashed@users.noreply.github.com>
2026-01-25 15:00:20 +0000

    deploy: 9c923da1c59fe77c3404520e0efc1a61dda339f2

--- assets/TaskTitle-D_zXbtzC.js
+++
@@ -1,1 +0,0 @@
-import{x as p,c as a,o as n,b as o,k as m,t as r}from"./index-CWn4r--5.js";const u={key:0},g={__name:"TaskTitle",props:{title:String},setup(c){const l=c,t=p(()=>String(l.title).split("->"));return(i,_)=>{var e,s;return n(),a("span",null,[o(r(t.value[0])+" ",1),(e=t.value)!=null&&e[1]?(n(),a("span",u,"»")):m("",!0),o(" "+r((s=t.value)==null?void 0:s[1]),1)])}}};export{g as _};
