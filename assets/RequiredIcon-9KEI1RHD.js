5f57fba80e575a5df9321b72106d03fde0a7f23f
MKRashed <MKRashed@users.noreply.github.com>
2026-01-27 11:55:00 +0000

    deploy: d58ebd9674fe8113fb5a805430b7cef3fde78273

---
+++ assets/RequiredIcon-9KEI1RHD.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./index-D8L4kBdR.js";const o={},s={class:"font-medium text-red-600 text-sm"};function n(r,_){return c(),t("span",s,"*")}const m=e(o,[["render",n]]);export{m as R};
