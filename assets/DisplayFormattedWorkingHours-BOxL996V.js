3d5ddec1a5bdf09e260eb89b790b13e711f04845
saifuddin24 <saifuddin24@users.noreply.github.com>
2026-02-01 06:26:06 +0000

    deploy: 0b6c77f08d5f5448b5d7913c748b45831c66af4c

---
+++ assets/DisplayFormattedWorkingHours-BOxL996V.js
@@ -0,0 +1,1 @@
+import{x as a,c as e,o as u,t as c}from"./index-DmUmJ6hT.js";const p={__name:"DisplayFormattedWorkingHours",props:["workingHours"],setup(n){const r=n,s=a(()=>{if(!r.workingHours)return"-";const o=Math.floor(r.workingHours),t=Math.round((r.workingHours-o)*60);return t?`${o}h ${t}m`:`${o}h`});return(o,t)=>(u(),e("span",null,c(s.value),1))}};export{p as _};
