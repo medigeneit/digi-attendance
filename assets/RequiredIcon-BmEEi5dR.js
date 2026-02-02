a7dd73ecc9953d386ba682581efaebbcb0f54622
saifuddin24 <saifuddin24@users.noreply.github.com>
2026-02-02 06:36:41 +0000

    deploy: bda1526a9ddd4524c2f35817b2c2695607c6d19d

--- assets/RequiredIcon-BmEEi5dR.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as c}from"./index-FCJsOcDw.js";const o={},s={class:"font-medium text-red-600 text-sm"};function n(r,_){return c(),t("span",s,"*")}const m=e(o,[["render",n]]);export{m as R};
