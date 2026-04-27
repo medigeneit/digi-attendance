c34ec5a2bbf54c8dbc82aa4e0f2b11a0a808fa0e
MKRashed <MKRashed@users.noreply.github.com>
2026-04-27 10:16:31 +0000

    deploy: e41c878a621f532f33542d6565f489734a83509d

---
+++ assets/RequiredIcon-eFyDS1-3.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./index-XZb0u_dE.js";const o={},s={class:"font-medium text-red-600 text-sm"};function n(r,_){return c(),t("span",s,"*")}const m=e(o,[["render",n]]);export{m as R};
