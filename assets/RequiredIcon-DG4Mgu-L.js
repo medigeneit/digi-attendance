705e2c1f48c2f08462905c1d4ab1f009205f9727
MKRashed <MKRashed@users.noreply.github.com>
2026-04-30 05:20:55 +0000

    deploy: 9258ed2ff89f948e71987f5b13ea2a8c487c36e9

---
+++ assets/RequiredIcon-DG4Mgu-L.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./index-W6l1xVpu.js";const o={},s={class:"font-medium text-red-600 text-sm"};function n(r,_){return c(),t("span",s,"*")}const m=e(o,[["render",n]]);export{m as R};
